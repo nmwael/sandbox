cde1b44ccfa91a15abef586b9e0f0ae5a04dc281
Matn <mniel@tdcnetdesign.dk>
2018-05-08 13:28:48 +0200

    auto generated spring boot project

---
+++ src/test/java/dk/netdesign/spring/pomwsdl/pomwsdl/PomwsdlApplicationTests.java
@@ -0,0 +1,16 @@
+package dk.netdesign.spring.pomwsdl.pomwsdl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PomwsdlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
